d2cd5a2c59f2bb5d69f5a6d337965854b7853991
Amy Balmforth <amy.balmforth@gmail.com>
2019-11-07 16:57:47 +0000

    creates instances of Bookmarks

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,19 +3,34 @@ require_relative 'database_test_helper'
 describe Bookmarks do
 
   context "#all" do
-    it "returns all the bookmarks" do
+    it "creates new instances of Bookmarks for each row with correct title" do
       add_bookmarks_for_test
-      expect(Bookmarks.all).to include(['http://www.maker.com',"maker"], ['http://www.youtube.com',"youtube"])
+      bookmark = Bookmarks.all
+      expect(bookmark[0].title).to eq "maker"
+      expect(bookmark[1].title).to eq "youtube"
+      expect(bookmark[2].title).to eq "facebook"
+    end
+
+    it "creates new instances of Bookmarks for each row with correct url" do
+      add_bookmarks_for_test
+      bookmark = Bookmarks.all
+      expect(bookmark[0].url).to eq "http://www.maker.com"
+      expect(bookmark[1].url).to eq "http://www.youtube.com"
+      expect(bookmark[2].url).to eq "http://www.facebook.com"
     end
   end
 
   context "#add_bookmarks" do
     it 'url and title gets added to database' do
       Bookmarks.add_bookmarks(url:"www.bbcnews.com", title:"lings")
-      expect(Bookmarks.all).to include(['www.bbcnews.com','lings'])
+      bookmark = Bookmarks.all
+      expect(bookmark[0].url).to eq "www.bbcnews.com"
+      expect(bookmark[0].title).to eq "lings"
     end
 
   end
+
+
 
 
 
